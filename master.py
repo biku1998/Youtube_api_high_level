fc064cc55732f451c1d003648cd365445a409eb6
biku1998 <kumarsourabh228@gmail.com>
2020-03-01 21:51:41 +0530

    Some .gitignore changes

--- master.py
+++ master.py
@@ -6,6 +6,9 @@ from youtube_lib.channel_id_collector import ChannelFetcher
 from youtube_lib.channel_video_collector import ChannelVideoCollector
 from youtube_lib.video_stats_collector import VideoStatsCollector
 
+
+
+
 if __name__ == '__main__':
     print('hello world')
 
